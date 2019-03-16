6fa64bdffddaeed668c3fd0449ddd903ef775f8a
rasalghul <liao13668009346@163.com>
2019-03-16 22:00:15 +0800

    3/16

--- Leetcpp/121.cpp
+++ Leetcpp/121.cpp
@@ -1,14 +1,15 @@
+
 //简单动态规划
-class Solution {
-public:
-	int maxProfit(vector<int>& prices) {
-		if (prices.size() == 0) return 0;
-		int mn = prices[0];
-		int mp = 0;
-		for (int i = 0; i < prices.size(); i++) {
-			mp = max(mp, prices[i] - mn);
-			mn = min(mn, prices[i]);
-		}
-		return mp;
-	}
-};
+//class Solution {
+//public:
+//	int maxProfit(vector<int>& prices) {
+//		if (prices.size() == 0) return 0;
+//		int mn = prices[0];
+//		int mp = 0;
+//		for (int i = 0; i < prices.size(); i++) {
+//			mp = max(mp, prices[i] - mn);
+//			mn = min(mn, prices[i]);
+//		}
+//		return mp;
+//	}
+//};
--- Leetcpp/test.cpp
+++ Leetcpp/test.cpp
@@ -3,50 +3,11 @@
 #include <string>
 #include <unordered_set>
 #include <stack>
+#include<algorithm>
 using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	/*TreeNode(int x) : val(x), left(NULL), right(NULL) {}*/
-};
-vector<int> inorderTraversal(TreeNode* root) {
-	vector<int> result;
-	stack<TreeNode*> s;
-	if (root == NULL)
-		return result;
-	TreeNode* p = root;
-	while (!s.empty() || p != NULL) {
-		if (p != NULL) {
-			// push 左子树入栈
-			s.push(p);
-			p = p->left;
-		}
-		else {
-			// 左子树为空时，访问该节点，然后访问右子树
-			p = s.top();
-			result.push_back(p->val);
-			s.pop();
-			p = p->right;
-		}
-	}
-	return result;
-}
 
 int main() {
-	TreeNode *A;
-	TreeNode *B;
-	A = (TreeNode*)malloc(sizeof(TreeNode));
-	A->val = 1;
-	A->left = (TreeNode*)malloc(sizeof(TreeNode));
-	A->right = NULL;
-	B = A->left;
-	B->val = 2;
-	B->left = (TreeNode*)malloc(sizeof(TreeNode));
-	B->right = NULL;
-	B->left->left = NULL;
-	B->left->right = NULL;
-	B->left->val = 3;
-	inorderTraversal(A);
-	int c = 0;
+	vector<int> a = {1,8,7,5,3,4};
+	sort(a.begin(), a.end());
+	
 }
